0798dccf6af3a79b0edfbc0f46a5b3f3055e9515
Jeff Seibert <jeff@jseibert.com>
2016-01-08 22:22:57 -0800

    Empty lines

--- lib/google-search/response.rb
+++ lib/google-search/response.rb
@@ -54,19 +54,23 @@ module Google
 
       def initialize hash
         @page = 0
+        @pages = []
         @hash = hash
         @size = (hash['responseSize'] || :large).to_sym
         @items = []
         @status = hash['responseStatus']
         @details = hash['responseDetails']
+
         if valid?
           if hash['responseData'].include? 'cursor'
             @estimated_count = hash['responseData']['cursor']['estimatedResultCount'].to_i
             @page = hash['responseData']['cursor']['currentPageIndex'].to_i
+            @pages = hash['responseData']['cursor']['pages']
           end
+
           @hash['responseData']['results'].each_with_index do |result, i|
             item_class = Google::Search::Item.class_for result['GsearchResultClass']
-            result['index'] = i + Google::Search.size_for(size) * page
+            result['index'] = i + @pages[page]['start'].to_i
             items << item_class.new(result)
           end
         end
@@ -84,9 +88,17 @@ module Google
       # Check if the response is valid.
 
       def valid?
-        hash['responseStatus'] == 200 && hash['responseData']['results'].count > 0
+        hash['responseStatus'] == 200 &&
+        hash['responseData']['results'].count > 0
       end
 
+      def has_next?
+        @page + 1 < @pages.count
+      end
+
+      def next_page_offset
+        @pages[@page + 1]['start'].to_i
+      end
     end
   end
 end

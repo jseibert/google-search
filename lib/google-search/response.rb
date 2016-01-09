6f49c83c9039990664a83127d082c711c75476d2
Jeff Seibert <jeff@jseibert.com>
2016-01-08 21:33:25 -0800

    Short-circuit search requests when there are no results:

--- lib/google-search/response.rb
+++ lib/google-search/response.rb
@@ -2,56 +2,56 @@
 module Google
   class Search
     class Response
-      
+
       #--
       # Mixins
       #++
-      
+
       include Enumerable
-      
+
       ##
       # Response status code.
-      
+
       attr_reader :status
-      
+
       ##
       # Response details.
-      
+
       attr_reader :details
-      
+
       ##
       # Raw JSON string.
-      
+
       attr_accessor :raw
-      
+
       ##
       # Hash parsed from raw JSON string.
-      
+
       attr_reader :hash
-      
+
       ##
       # Items populated by the JSON hash.
-      
+
       attr_reader :items
-      
+
       ##
       # Estimated number of results.
-      
+
       attr_reader :estimated_count
-      
+
       ##
       # Current page index.
-      
+
       attr_reader :page
-      
+
       ##
       # Size of response.
-      
+
       attr_reader :size
 
       ##
       # Initialize with _hash_.
-      
+
       def initialize hash
         @page = 0
         @hash = hash
@@ -71,22 +71,22 @@ module Google
           end
         end
       end
-      
+
       ##
       # Iterate each item with _block_.
-      
+
       def each_item &block
         items.each { |item| yield item }
       end
       alias :each :each_item
-      
+
       ##
       # Check if the response is valid.
-      
+
       def valid?
-        hash['responseStatus'] == 200
+        hash['responseStatus'] == 200 && hash['responseData']['results'].count > 0
       end
-      
+
     end
   end
 end
